db0924360dfe6c33d474c8a790b1e9c0e443d7e0
Nelson Diaz <abnerdc86@gmail.com>
2018-06-08 17:17:30 -0400

    decoding HTML with "he" library

--- utils/scraper.js
+++ utils/scraper.js
@@ -2,6 +2,8 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const mongoose = require('mongoose');
 const db = require('../models');
+// Library used to decode the HTML into plain text string
+const he = require('he');
 
 function scrapeArticles(cb) {
 	var url = 'http://www.miamiherald.com/news/';
@@ -13,16 +15,16 @@ function scrapeArticles(cb) {
 
 			$('#story-list .teaser').each(function (i, element) {
 				var article = {
-					title: $(this).find('h4 > a').html().trim(),
+					title: he.decode($(this).find('h4 > a').html().trim()),
 					link: $(this).find('a').attr('href').trim(),
-					summary: $(this).find('p.summary').html().trim()
+					summary: he.decode($(this).find('p.summary').html().trim())
 				}
-				// articleList.push(article);
 				db.Article.create(article)
-					.then(function (data) {
-						// If saved successfully, print the new Library document to the console
-						console.log("Article successfully stored in the Database \n" + data);
-					})
+				.then(function (data) {
+					// If saved successfully, print the new Library document to the console
+					console.log("Article successfully stored in the Database \n" + data);
+					articleList.push(data);
+				})
 					.catch(function (err) {
 						console.log(err.message);
 					});
