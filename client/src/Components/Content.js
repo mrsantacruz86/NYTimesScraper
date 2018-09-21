adcd55aa479db4dec510cc5e6ceff44b86a3768f
Nelson Diaz <abnerdc86@gmail.com>
2018-09-21 04:12:38 -0400

    create content component

---
+++ client/src/Components/Content.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Article from './Article';
+import articleList from '../js/dummyData';
+
+class Content extends Component {
+
+	render() {
+		return (
+			<div className="row">
+				<div className="panel panel-default">
+					<div className="panel-heading">
+						<h3 className="panel-title">List of Articles</h3>
+					</div>
+					<div className="panel-body">
+						<ul className="list-group">
+							{articleList.map(item => (
+								<Article article={item} key={item._id} />
+							))}
+						</ul>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Content;
