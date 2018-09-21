2acb6660255e2255798eb551fbfc802ec54f1c5a
Nelson Diaz <abnerdc86@gmail.com>
2018-09-21 16:11:26 -0400

    set up proxy, eslint, dev and prod env.

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 const logger = require('morgan');
-const exphbs = require('express-handlebars');
 const routes = require('./config/routes');
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3001;
 //create an express server
 var app = express();
 app.use(logger('dev'));
@@ -16,7 +15,7 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,9 +23,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
-// Set Handlebars.
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 app.use(routes);
