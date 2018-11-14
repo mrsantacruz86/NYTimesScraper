d10bec3567abad2093c386682cfa02eb0b3ab513
Nelson Diaz <abnerdc86@gmail.com>
2018-11-14 03:47:45 -0500

    trying a different approach for the notes controller. not working yet

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -4,19 +4,17 @@ const Article = require('../models/Article');
 
 module.exports = {
 	create: (data, cb) => {
-		console.log(data);
+		console.log("Printing Data", data);
 		let note = new Note(data);
-		console.log(note);
+		console.log("Printing Note", note);
 		note.save(err => {
 			if (err) return console.log(err);
-			Article.findById(note._articleId, (err, response) => {
-				if (err) return console.log(err);
-				let article = response;
-				article.notes.push(note._id);
-				console.log("Returning article found", article);
-				Article.update({_id: article._Id}, { $set: article }, cb(note));
-			});
+			console.log("Notes was created");
 		});
+		Article.findByIdAndUpdate(note._articleId,
+			{ $push: { notes: note._id } },
+			cb(note));
+			console.log("Note's reference was added to the article" );
 	},
 	delete: (data, cb) => Note.remove({ _id: data._id }, cb),
 	get: (data, cb) => Note.find({ _articleId: data._id }, cb),
