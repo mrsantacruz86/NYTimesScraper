b9d2f9a2fa0fb7eb6a08334b77bde10bad4f6a1d
Nelson Diaz <abnerdc86@gmail.com>
2018-11-22 00:11:35 -0500

    changing redux logger for redux dev tools

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,14 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
-import { logger } from 'redux-logger';
+// import { logger } from 'redux-logger';
 import thunk from 'redux-thunk';
 
+const middleware = [thunk];
 
+/* eslint-disable no-underscore-dangle */
 export default createStore(
   rootReducer,
-  applyMiddleware(logger, thunk)
+  compose(applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
+/* eslint-enable */
