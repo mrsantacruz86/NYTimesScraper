ebc8b3b3594a51fc1f5b431d8f80b2e54590c0e6
Nelson Diaz <abnerdc86@gmail.com>
2018-11-30 16:43:35 -0500

    adjust stiles for the jumbotron

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -8,8 +8,8 @@ class Header extends Component {
 				<header className="parallax-bg bg1">
 					<div className="jumbotron page-banner">
 						<div className="container-fluid">
-							<h1 className="text-center custom-title">MONGO SCRAPER</h1>
-							<h2 className="text-center custom-title">New York Times Scraper</h2>
+							<h1 className="text-center custom-title display-4">NEWS SCRAPER</h1>
+							<h1 className="text-center custom-title">New York Times</h1>
 							<br />
 						</div>
 					</div>
