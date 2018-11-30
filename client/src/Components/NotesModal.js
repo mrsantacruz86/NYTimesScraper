2cb71b31c9ecc8381965713d0fd5fb6bacd95297
Nelson Diaz <abnerdc86@gmail.com>
2018-11-30 16:44:06 -0500

    working on the form to enter notes.

--- client/src/Components/NotesModal.js
+++ client/src/Components/NotesModal.js
@@ -28,9 +28,13 @@ class NotesModal extends Component {
 	}
 	onChange = (e) => {
 		e.preventDefault();
+		this.setState({
+			[e.target.name]: e.target.value
+		});
 	}
 	onSubmit = (e) => {
 		e.preventDefault();
+		this.setState({text:""});
 	}
 
 	render() {
@@ -40,36 +44,50 @@ class NotesModal extends Component {
 		return (
 			<div>
 				<Modal size="lg" isOpen={this.props.articles.articleDetailModal} toggle={this.toggle} className="notes-modal">
-					<ModalHeader toggle={this.toggle}>Article: {_id}</ModalHeader>
-					<ModalBody>
+					<ModalHeader toggle={this.toggle}>
 						<Row>
 							<Col>
-								<Form inline>
-									<FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-										<Label for="exampleEmail" className="mr-sm-2">Email</Label>
-										<Input type="text" name="email" id="exampleEmail" placeholder="something@idk.cool" />
-									</FormGroup>
-									<Button>Submit</Button>
-								</Form>
+								Article: {_id}
 							</Col>
 						</Row>
-
-						<ListGroup>
-							{notes && notes.length > 0 ?
-								notes.map(note =>
-									<ListGroupItem>
-										<ListGroupItemHeading>
-											{note.date}
-										</ListGroupItemHeading>
-										<ListGroupItemText>
-											{note.text}
-										</ListGroupItemText>
-									</ListGroupItem>
-								)
-								:
-								<p>There are no Notes to Display at this time</p>
-							}
-						</ListGroup>
+					</ModalHeader>
+					<ModalBody>
+						<Form onSubmit={this.onSubmit} >
+							<Row>
+								<Col>
+									<FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+										<Label for="noteInput" hidden >Note</Label>
+										<Input type="textarea" name="text" id="noteInput" placeholder="Add a note here..." onChange={this.onChange} value={this.state.text} />
+									</FormGroup>
+								</Col>
+							</Row>
+							<Row>
+								<Col style={{marginTop: 10}}>
+									<Button>Add Note</Button>
+								</Col>
+							</Row>
+						</Form>
+						<hr />
+						<Row>
+							<Col>
+								<ListGroup>
+									{notes && notes.length > 0 ?
+										notes.map(note =>
+											<ListGroupItem>
+												<ListGroupItemHeading>
+													{note.date}
+												</ListGroupItemHeading>
+												<ListGroupItemText>
+													{note.text}
+												</ListGroupItemText>
+											</ListGroupItem>
+										)
+										:
+										<p>There are no Notes to Display at this time</p>
+									}
+								</ListGroup>
+							</Col>
+						</Row>
 					</ModalBody>
 					<ModalFooter>
 						<Button
