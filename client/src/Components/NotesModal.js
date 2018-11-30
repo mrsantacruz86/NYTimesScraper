108ea691eee72593af2df254c980c679bdb162a9
Nelson Diaz <abnerdc86@gmail.com>
2018-11-30 01:59:30 -0500

    working ont the notes form #1

--- client/src/Components/NotesModal.js
+++ client/src/Components/NotesModal.js
@@ -7,6 +7,10 @@ import {
 	ModalHeader,
 	ModalBody,
 	ModalFooter,
+	ListGroup,
+	ListGroupItem,
+	ListGroupItemHeading,
+	ListGroupItemText,
 	Form,
 	FormGroup,
 	Label,
@@ -20,34 +24,54 @@ class NotesModal extends Component {
 	toggle = () => {
 		this.props.toggleArticleDetails();
 	}
+	onChange = (e) => {
+		e.preventDefault();
+	}
+	onSubmit = (e) => {
+		e.preventDefault();
+	}
 
 	render() {
 		const {
-			_id,
-			notes,
-			link,
-			title,
-			summary,
-			saved
+			_id, notes, link, title, summary, saved
 		} = this.props.articles.selectedArticle;
 		return (
 			<div>
-				<Modal isOpen={this.props.articles.articleDetailModal} toggle={this.toggle} className="notes-modal">
+				<Modal size="lg" isOpen={this.props.articles.articleDetailModal} toggle={this.toggle} className="notes-modal">
 					<ModalHeader toggle={this.toggle}>Article: {_id}</ModalHeader>
 					<ModalBody>
-						<ul>
+						<Form inline>
+							<FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+								<Label for="exampleEmail" className="mr-sm-2">Email</Label>
+								<Input type="email" name="email" id="exampleEmail" placeholder="something@idk.cool" />
+							</FormGroup>
+							<Button>Submit</Button>
+						</Form>
+
+						<ListGroup>
 							{notes && notes.length > 0 ?
 								notes.map(note =>
-									<li>{note.text}<br />{note.date}</li>
+									<ListGroupItem>
+										<ListGroupItemHeading>
+											{note.date}
+										</ListGroupItemHeading>
+										<ListGroupItemText>
+											{note.text}
+										</ListGroupItemText>
+									</ListGroupItem>
 								)
 								:
 								<p>There are no Notes to Display at this time</p>
 							}
-						</ul>
+						</ListGroup>
 					</ModalBody>
 					<ModalFooter>
-						<Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-						<Button color="secondary" onClick={this.toggle}>Cancel</Button>
+						<Button
+							color="secondary"
+							onClick={this.toggle}
+						>
+							Cancel
+						</Button>
 					</ModalFooter>
 				</Modal>
 			</div>
