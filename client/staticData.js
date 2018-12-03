8fe07af92893d4670091a5739f50560b5b3a001c
Nelson Diaz <abnerdc86@gmail.com>
2018-12-03 01:30:29 -0500

    updating seeds file.

---
+++ client/staticData.js
@@ -0,0 +1,113 @@
+export const articles = [
+  {
+    saved: true,
+    notes: [
+      "5c023ce293416c6777170fc9",
+      "5c023e3893416c6777170fca",
+      "5c023e5a93416c6777170fcb",
+      "5c023e6593416c6777170fcc",
+      "5c023f1b93416c6777170fcd"
+    ],
+    _id: "5c01f39ef489a861bc789348",
+    title: "There Will Be Blood-Red Trees",
+    link: "https://www.nytimes.com/2018/11/30/fashion/white-house-christmas-decorations.html",
+    summary: "First Lady Melania Trump has defended this year’s White House stark tidings.",
+    date: "2018-12-01T02:36:14.505Z"
+  },
+  {
+    saved: true,
+    notes: [],
+    _id: "5c01f39ef489a861bc789347",
+    title: "The Many Times Trump Denied Ties to Russia",
+    link: "https://www.nytimes.com/video/us/politics/100000006240299/trump-russia-denials.html",
+    summary: "During the 2016 presidential campaign, Donald Trump repeatedly denied having business ties to Russia. Now that the president’s former lawyer has confessed to lying to Congress about a plan to build a tower in Russia, Trump’s story has changed.",
+    date: "2018-12-01T02:36:14.505Z"
+  },
+  {
+    saved: false,
+    notes: [],
+    _id: "5c01f39ef489a861bc789346",
+    title: "Ed Pastor, Arizona’s First Hispanic Member of Congress, Dies at 75",
+    link: "https://www.nytimes.com/2018/11/30/obituaries/ed-pastor-dead.html",
+    summary: "A son of a miner, he served 23 years in the House, championing immigration and education reforms as well as universal health care.",
+    date: "2018-12-01T02:36:14.505Z"
+  },
+  {
+    saved: true,
+    notes: [],
+    _id: "5c01f39ef489a861bc789345",
+    title: "North Carolina Election That Looked to Be Republican Victory Now in Limbo",
+    link: "https://www.nytimes.com/2018/11/30/us/politics/north-carolina-vote-fraud.html",
+    summary: "The state elections board said it would not certify the results in the Ninth Congressional District and instead would hold a December hearing to explore claims of fraudulent absentee ballots.",
+    date: "2018-12-01T02:36:14.505Z"
+  },
+  {
+    saved: false,
+    notes: [],
+    _id: "5c01f39ef489a861bc789344",
+    title: "Dodging Friends, Chased by Legal Troubles, Trump Navigates G-20",
+    link: "https://www.nytimes.com/2018/11/30/world/americas/trump-g20-putin-prince.html",
+    summary: "At the Group of 20 gathering in Buenos Aires, President Trump declined to meet with his two favorite strongmen, postponed meeting an ally and was preoccupied with the Russia investigation.",
+    date: "2018-12-01T02:36:14.504Z"
+  },
+  {
+    saved: false,
+    notes: [],
+    _id: "5c01f39ef489a861bc789343",
+    title: "Homeland Security Dept. Request Would Keep Troops on Border Through January",
+    link: "https://www.nytimes.com/2018/11/30/us/politics/military-troops-border-caravan.html",
+    summary: "A Pentagon official said Defense Secretary Jim Mattis was expected to approve the request, which would keep at least some of the military forces on the border for the Christmas holidays.",
+    date: "2018-12-01T02:36:14.504Z"
+  },
+  {
+    saved: true,
+    notes: [],
+    _id: "5c01f39ef489a861bc789342",
+    title: "Trump’s Book Club: A President Who Doesn’t Read Promotes the Books That Promote Him",
+    link: "https://www.nytimes.com/2018/11/30/us/politics/trump-books.html",
+    summary: "President Trump, who is not a reader, has used Twitter to plug a slew of Trump-friendly books with titles like “Why We Fight” and “The Russia Hoax.”",
+    date: "2018-12-01T02:36:14.504Z"
+  },
+  {
+    saved: false,
+    notes: [],
+    _id: "5c01f39ef489a861bc789341",
+    title: "Agency Pulls Back on Its Warning Against Talk of ‘Resistance’ in Federal Workplaces",
+    link: "https://www.nytimes.com/2018/11/30/us/politics/federal-employees-trump-impeachment-resist.html",
+    summary: "Casual conversations about impeachment and invocations of “resistance” to policies, but not to President Trump, will be permitted, the Office of Special Counsel said.",
+    date: "2018-12-01T02:36:14.503Z"
+  },
+  {
+    saved: false,
+    notes: [],
+    _id: "5c01f39ef489a861bc789340",
+    title: "A China Hawk Gains Prominence as Trump Confronts Xi on Trade",
+    link: "https://www.nytimes.com/2018/11/30/us/politics/trump-china-trade-xi-michael-pillsbury.html",
+    summary: "Michael Pillsbury, the president’s top outside adviser on China, has the president’s ear ahead of negotiations with Xi Jinping.",
+    date: "2018-12-01T02:36:14.503Z"
+  },
+  {
+    saved: true,
+    notes: [
+      "5c00d1003fba8942621d872d",
+      "5c00d1043fba8942621d872e",
+      "5c00d1073fba8942621d872f",
+      "5c0239b793416c6777170fc7",
+      "5c023c8593416c6777170fc8"
+    ],
+    _id: "5c00a8823fba8942621d8726",
+    title: "Trump Denies Business Dealings With Russia. His Former Lawyer Contradicts Him.",
+    link: "https://www.nytimes.com/2018/11/29/us/politics/fact-check-cohen-trump-.html",
+    summary: "For years, the president has denied that he had any business interests in Russia during the 2016 election. His former personal lawyer now says otherwise.",
+    date: "2018-11-30T03:03:30.877Z"
+  },
+  {
+    saved: false,
+    notes: [],
+    _id: "5c00a8823fba8942621d8725",
+    title: "Pelosi’s Strategy in Secret Ballot: Allow Her Critics to Let Off Steam",
+    link: "https://www.nytimes.com/2018/11/29/us/politics/pelosi-speaker-vote.html",
+    summary: "Nancy Pelosi gave her opponents a chance to cast a protest vote against her as she runs for House speaker, hoping that they would change course before a formal vote in January.",
+    date: "2018-11-30T03:03:30.877Z"
+  },  
+];
